24e8b618357fbdf066d43a21a7c0f75eb19ddfd2
Marcus Ikegami <hikegami.6@gmail.com>
2021-09-20 12:57:08 -0600

    Added a function and used several methods to learn some basic js.

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+var playerName = window.prompt("What is your robot's name?");
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight(); 
+console.log(playerName);
+
+console.log("this is a string");
+console.log(10 + 10);
+console.log ("Our robot's name is " + playerName + " " + (10 + 10));
